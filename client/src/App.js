1a3f50130982f862f124a8ef23abc4509bed64bd
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-20 11:31:28 -0700

    wrapped App in Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import { render } from 'react-dom'
 
 import SavedList from './Movies/SavedList';
-import { BrowserRouter as Router } from 'react-router-dom'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -12,12 +10,10 @@ const App = () => {
   };
 
   return (
-    <Router>
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
     </div>
-    </Router>
   );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,17 @@
+// 👉 Importing React libs from node_modules folder
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { ReactDOM } from 'react-dom';
 
+// 👉 STEP 1 - Importing React Router's Router
+import { BrowserRouter as Router } from 'react-router-dom'
+
+// 👉 Importing the top-level components
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+     <Router>
+          <App />
+     </Router>
+     , document.getElementById('root'));
