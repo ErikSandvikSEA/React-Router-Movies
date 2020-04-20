a42288888923e590f0443ed9badbeb303604d8e2
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-04-20 13:50:04 -0700

    added MovieCard stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 // STEP 2 - import Route, Switch and Link from react router
-import { Route, Switch, Link, useParams } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import SavedList from './Movies/SavedList'
 import MovieList from './Movies/MovieList'
@@ -8,7 +8,6 @@ import Movie from './Movies/Movie'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
-  const { id } = useParams()
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, useHistory, useRouteMatch, Route, Link } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 
 const SavedList = props => {
